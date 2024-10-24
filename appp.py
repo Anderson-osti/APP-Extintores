9a69669f36f81dc5d6c66528f51c907589e0f909
Anderson-osti <andosti@hotmail.com>
2024-10-24 12:12:02 -0300

    Add files via upload

--- appp.py
+++ appp.py
@@ -14,6 +14,7 @@ def criar_conexao():
         return None
 
 def verificar_usuario(username, senha):
+    # Usuários permitidos
     usuarios_permitidos = {
         st.secrets["USUARIO1"]: st.secrets["SENHA1"],
         st.secrets["USUARIO2"]: st.secrets["SENHA2"]
@@ -32,7 +33,7 @@ def cadastrar_empresa(nome_empresa, endereco, tipo_extintor, quantidade_extintor
             "tipo_extintor": tipo_extintor,
             "quantidade_extintor": quantidade_extintor,
             "capacidade_extintor": capacidade_extintor,
-            "data_cadastro": data_cadastro.isoformat()  # Convertendo para string
+            "data_cadastro": data_cadastro.strftime('%Y-%m-%d')  # Convertendo para string
         }
         db.empresas.insert_one(empresa)
         st.success("Empresa cadastrada com sucesso!")
@@ -46,76 +47,15 @@ def listar_empresas():
 
     try:
         empresas = db.empresas.find()
-        return list(empresas)
+        empresas_list = list(empresas)
+        st.write("Empresas Cadastradas:", empresas_list)  # Mostra o conteúdo retornado
+        return empresas_list
     except Exception as e:
         st.error(f"Erro ao listar empresas: {e}")
         return []
 
-def excluir_empresa(nome_empresa):
-    db = criar_conexao()
-    if db is None:
-        return
-
-    try:
-        result = db.empresas.delete_one({"nome_empresa": nome_empresa})
-        if result.deleted_count > 0:
-            st.success("Empresa excluída com sucesso!")
-        else:
-            st.warning("Nenhuma empresa encontrada com esse nome.")
-    except Exception as e:
-        st.error(f"Erro ao excluir empresa: {e}")
-
-def gerar_pdf(empresas):
-    class PDF(FPDF):
-        def header(self):
-            self.set_font('Arial', 'B', 12)
-            self.cell(0, 10, 'Relatório de Empresas Cadastradas', 0, 1, 'C')
-            self.ln(10)
-
-        def footer(self):
-            self.set_y(-15)
-            self.set_font('Arial', 'I', 8)
-            self.cell(0, 10, f'Página {self.page_no()}', 0, 0, 'C')
-
-        def chapter_title(self, title):
-            self.set_font('Arial', 'B', 12)
-            self.cell(0, 10, title, 0, 1, 'L')
-            self.ln(5)
-
-        def chapter_body(self, body):
-            self.set_font('Arial', '', 12)
-            self.multi_cell(0, 10, body)
-            self.ln()
-
-    pdf = PDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-
-    for empresa in empresas:
-        pdf.chapter_title(f"Empresa: {empresa['nome_empresa']}")
-        body = (
-            f"Endereço: {empresa['endereco']}\n"
-            f"Tipo de Extintor: {empresa['tipo_extintor']}\n"
-            f"Quantidade: {empresa['quantidade_extintor']}\n"
-            f"Capacidade: {empresa['capacidade_extintor']}\n"
-            f"Data de Cadastro: {empresa['data_cadastro']}"
-        )
-        pdf.chapter_body(body)
-
-    pdf_file = "relatorio_empresas.pdf"
-    pdf.output(pdf_file)
-
-    with open(pdf_file, "rb") as file:
-        btn = st.download_button(
-            label="Baixar Relatório em PDF",
-            data=file,
-            file_name=pdf_file,
-            mime="application/octet-stream"
-        )
-    st.success("PDF gerado com sucesso!")
-
 def tela_login():
-    st.image('logo.png', width=100)
+    st.image('logo.png', width=100)  # Adicionando o logotipo
     st.title("Login no Meu App")
     username = st.text_input("Usuário", key="username")
     senha = st.text_input("Senha", type="password", key="senha")
@@ -123,9 +63,7 @@ def tela_login():
     if st.button("Login"):
         if verificar_usuario(username, senha):
             st.session_state['logged_in'] = True
-            st.session_state['username'] = username  # Armazenar usuário logado
-            st.success("Login bem-sucedido!")
-            st.experimental_rerun()
+            st.rerun()
         else:
             st.error("Usuário ou senha incorretos.")
 
@@ -133,60 +71,34 @@ def tela_cadastro():
     st.header("Cadastro de Empresa")
     nome_empresa = st.text_input("Nome da Empresa", key="nome_empresa")
     endereco = st.text_input("Endereço", key="endereco")
-    tipo_extintor = st.selectbox("Tipo de Extintor", ["Água", "Pó Químico (BC)", "Pó Químico (ABC)", "CO2", "Espuma"], key="tipo_extintor")
+    tipo_extintor = st.selectbox("Tipo de Extintor", ["Água", "Pó Químico (BC)", "Pó Químico (ABC)", "CO2", "Espuma"],
+                                 key="tipo_extintor")
     quantidade_extintor = st.number_input("Quantidade de Extintores", min_value=1, step=1, key="quantidade_extintor")
-    capacidade_extintor = st.selectbox("Capacidade do Extintor", ["4 kg", "6 kg", "9 kg", "12 kg", "6 L", "10 L"], key="capacidade_extintor")
+    capacidade_extintor = st.selectbox("Capacidade do Extintor", ["4 kg", "6 kg", "9 kg", "12 kg", "6 L", "10 L"],
+                                       key="capacidade_extintor")
     data_cadastro = st.date_input("Data de Cadastro", datetime.now(), key="data_cadastro")
 
     if st.button("Cadastrar Empresa"):
         if nome_empresa and endereco:
-            cadastrar_empresa(nome_empresa, endereco, tipo_extintor, quantidade_extintor, capacidade_extintor, data_cadastro)
+            cadastrar_empresa(nome_empresa, endereco, tipo_extintor, quantidade_extintor, capacidade_extintor,
+                              data_cadastro)
         else:
             st.error("Por favor, preencha todos os campos obrigatórios.")
 
-def tela_excluir_empresa():
-    st.header("Excluir Empresa")
-    empresas = listar_empresas()
-
-    if not empresas:
-        st.warning("Nenhuma empresa cadastrada para excluir.")
-        return
-
-    nomes_empresas = [empresa['nome_empresa'] for empresa in empresas]
-    nome_empresa = st.selectbox("Selecione a Empresa a ser excluída", nomes_empresas, key="nome_empresa_excluir")
-
-    if st.button("Excluir Empresa"):
-        excluir_empresa(nome_empresa)
-
-def tela_relatorio():
-    st.header("Gerar Relatório de Empresas")
-    if st.button("Gerar Relatório"):
-        empresas = listar_empresas()
-        if empresas:
-            gerar_pdf(empresas)
-        else:
-            st.warning("Nenhuma empresa cadastrada para gerar o relatório.")
-
 def menu_principal():
     st.sidebar.title("Menu")
-    opcao = st.sidebar.selectbox("Escolha uma opção", ["Cadastro de Empresa", "Listar Empresas", "Excluir Empresa", "Gerar Relatório", "Sair"])
+    opcao = st.sidebar.selectbox("Escolha uma opção",
+                                 ["Cadastro de Empresa", "Listar Empresas Cadastradas"], key="menu_opcao")
 
     if opcao == "Cadastro de Empresa":
         tela_cadastro()
-    elif opcao == "Listar Empresas":
+    elif opcao == "Listar Empresas Cadastradas":
         empresas = listar_empresas()
         if empresas:
             for empresa in empresas:
-                st.write(f"Nome: {empresa['nome_empresa']}, Endereço: {empresa['endereco']}, Tipo: {empresa['tipo_extintor']}, Quantidade: {empresa['quantidade_extintor']}, Capacidade: {empresa['capacidade_extintor']}, Data de Cadastro: {empresa['data_cadastro']}")
+                st.write(f"Nome: {empresa['nome_empresa']}, Endereço: {empresa['endereco']}")
         else:
-            st.write("Nenhuma empresa cadastrada.")
-    elif opcao == "Excluir Empresa":
-        tela_excluir_empresa()
-    elif opcao == "Gerar Relatório":
-        tela_relatorio()
-    elif opcao == "Sair":
-        st.session_state['logged_in'] = False
-        st.experimental_rerun()
+            st.warning("Nenhuma empresa cadastrada.")
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
